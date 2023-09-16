1bc81115d8e1582db6ff37064ea5e8dcd02bc714
Rod512 <rudro149187@bsdi-bd.org>
2023-09-17 03:16:54 +0600

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Fragment } from 'react';
 import './App.css'
+import { Route, Router, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Homepage from './pages/Homepage';
+import Coinpage from './pages/Coinpage';
+import { Box, createTheme } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box sx={{
+        backgroundColor: '#14161a',
+        color:'white',
+        minHeight: '100vh'
+      }}>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Homepage />} exact/>
+          <Route path='/coins/:id' element={<Coinpage/>}/>
+        </Routes>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Coinpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coinpage = () => {
+    return (
+        <div>
+            <h1>Coinpage</h1>
+        </div>
+    );
+};
+
+export default Coinpage;
