541eed265ce412b2a21beb4140aae74d9899eba5
Rod512 <rudro149187@bsdi-bd.org>
2023-09-19 00:21:14 +0600

    navbar done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,42 @@
+import { AppBar, Container, MenuItem, Select, ThemeProvider, Box, Typography, createTheme, FormControl, InputLabel } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const Header = () => {
+    const darkTheme = createTheme({
+        palette:{
+           mode:'dark',
+        }
+    })
+    
     return (
         <div>
-            <h1>Header</h1>
+            <ThemeProvider theme={darkTheme}>
+                <AppBar color='transparent' position='static'>
+                    <Container>
+                        <Box sx={{display:'flex', alignItems:'center', justifyContent:'space-between',}}>
+                            <Typography sx={{color:'gold',fontWeight:"bold"}}>
+                                <Link to={'/'}>Crypto Hunter</Link>
+                            </Typography>
+
+                            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                            <InputLabel id="demo-select-small-label">Currency</InputLabel>
+                            <Select
+                              labelId="demo-select-small-label"
+                              id="demo-select-small"
+                              label="Currency"
+                            >
+                              <MenuItem value={'USD'}>USD</MenuItem>
+                              <MenuItem value={'INR'}>INR</MenuItem>
+                            </Select>
+                          </FormControl>
+
+                        </Box>
+                    </Container>
+                </AppBar>
+            </ThemeProvider>
         </div>
     );
 };
