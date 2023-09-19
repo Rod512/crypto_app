fb27eff3389467fb86340947175e823902151bed
Rod512 <rudro149187@bsdi-bd.org>
2023-09-19 14:04:27 +0600

    context api done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import { AppBar, Container, MenuItem, Select, ThemeProvider, Box, Typography, createTheme, FormControl, InputLabel } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CryptoState } from '../CryptoContex';
 
 
 
@@ -10,6 +11,9 @@ const Header = () => {
            mode:'dark',
         }
     })
+
+    const {currency,setCurrency}= CryptoState()
+    console.log(currency)
     
     return (
         <div>
@@ -17,7 +21,7 @@ const Header = () => {
                 <AppBar color='transparent' position='static'>
                     <Container>
                         <Box sx={{display:'flex', alignItems:'center', justifyContent:'space-between',}}>
-                            <Typography sx={{color:'gold',fontWeight:"bold"}}>
+                            <Typography sx={{color:'gold',fontWeight:"bold"}} variant='h6'>
                                 <Link to={'/'}>Crypto Hunter</Link>
                             </Typography>
 
@@ -27,9 +31,12 @@ const Header = () => {
                               labelId="demo-select-small-label"
                               id="demo-select-small"
                               label="Currency"
+
+                              value={currency}
+                              onChange={(e)=>setCurrency(e.target.value)}
                             >
                               <MenuItem value={'USD'}>USD</MenuItem>
-                              <MenuItem value={'INR'}>INR</MenuItem>
+                              <MenuItem value={'BDT'}>BDT</MenuItem>
                             </Select>
                           </FormControl>
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import CryptoContex from './CryptoContex.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
-        <App/>
+        <CryptoContex>
+            <App/>
+        </CryptoContex>
     </BrowserRouter>
     
-   
 )
